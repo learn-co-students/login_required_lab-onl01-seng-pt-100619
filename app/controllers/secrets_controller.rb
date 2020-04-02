0cbe61aa7d4ab298a63b9c76f4363fecf41b32e7
wendyv510 <wendyv510@hotmail.com>
2020-04-02 14:51:09 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,14 @@
+class SecretsController < ApplicationController
+       
+
+     def show 
+        if current_user 
+            render :show 
+        else 
+            redirect_to '/new' 
+        end 
+    end 
+
+    
+
+end
